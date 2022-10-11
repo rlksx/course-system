5730178db053a3e4e025aaf20ae68ac9223f5d43
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-10-11 02:11:17 -0300

    add-system-exception

--- ContentContext/CareerItem.cs
+++ ContentContext/CareerItem.cs
@@ -3,10 +3,20 @@ namespace course_system.ContentContext
     public class CareerItem 
     // classe item da carreira que organiza o cursos da carreira;
     {
-        public int Order { get; set; } // ordem dos cursos;
-        public string Title { get; set; } // Titulo da carreira;
-        public string Description { get; set; } // descrição da carreira;
-        public Course Courses { get; set; } // Cursos da carreira;
+        public int Order { get; set; } // ordem do curso;
+        public string Title { get; set; } // Titulo do curso;
+        public string Description { get; set; } // descrição da curso
+        public Course Course { get; set; } // curso;
 
+        public CareerItem(int order, string title, string description, Course course)
+        {
+            this.Order = order;
+            this.Title = title;
+            this.Description = description;
+            this.Course = course;
+
+            if (Course == null)
+                throw new System.Exception("O curso não pode ser nulo!");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,11 +8,45 @@ namespace course_system {
             Console.Clear();
 
             // carreira back end;
-            var backend = new Career("Carreira Backend com .NET", null);
-            // items da carreira back end, como titulo, descrição e cursos;
-            backend.Items.Add(new CareerItem());
+            var careers = new List<Career>();
+            var backend = new Career("Carreira Backend com .NET", "Neste curso você vai aprender os fundamentos da programação com C#");
+
+            // cursos da carreiras;
+            var courses = new List<Course>(); 
+            var course01 = new Course("Fundamentos do C#", "csharp-fundamentals");
+            var course02 = new Course("Programação Orientdada a Objetos", "oop-course");
+            var course03 = new Course("SQL Server", "sqlserver-course");  
+
+            // adicionando a lista de cursos;
+            courses.Add(course01);
+            courses.Add(course02);
+            courses.Add(course03);
+            
+            // criando items da carreira e adicionand cursos á carreira
+            var fundamentalsCsharp = new CareerItem(1, "Fundamentos C#", "0", course01); // curso da carreira 
+            var OOP = new CareerItem(2, "Programação Orientada a Objeto", "0", course02); // curso da carreira 
+            var SQLSever = new CareerItem(3, "SQL Sever", "0", course03); // curso da carreira 
+
+            // adicionando items da carreira back end, como titulo, descrição e cursos;
+            backend.Items.Add(SQLSever); // ordem 3
+            backend.Items.Add(OOP); // ordem 2
+            backend.Items.Add(fundamentalsCsharp); // ordem 1
+
+            // adicionando carreias na lista de carreiras;
+            careers.Add(backend);
+
             // quantidade de cursos da carreira back end;
-            Console.Write($"A carreira backend possuí {backend.TotalCourses} cursos!\n");
+            Console.Write($"A carreira backend possuí {backend.TotalCourses} curso(s)!\n");
+            
+            // ordem dos cursos
+            foreach (var career in careers) {
+                Console.WriteLine(career.Title);
+                foreach (var item in career.Items.OrderBy(x => x.Order) ) { // OrderBy e OrderByDeceding;
+                    // ordenando cursos ddas carreiras
+                    Console.WriteLine($"{item.Order} - {item.Title} - Nivel: {item.Course.Level}"); 
+                }
+            }
+
 
             // artigos
             var articles = new List<Article>();
